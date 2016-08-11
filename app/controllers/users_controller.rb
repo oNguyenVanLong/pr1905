15215f5027e8db76356715061d4a1195e4ce06ff
longnv5994 <longnv5994@gmail.com>
2016-08-11 10:26:55 +0700

    Check convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,13 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
 
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+        :password_confirmation)
     end
 end
