b62035d4f643ac9f29978cab2e33188a9aa4bcc3
longnv5994 <longnv5994@gmail.com>
2016-08-17 22:22:51 +0700

    blog unsuscess

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,21 +3,13 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find_by id: params[:followed_id]
-    current_user.follow @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
-
+    current_user.follow user
+    redirect_to user
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
-    current_user.unfollow @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
-
+    current_user.unfollow user
+   redirect_to user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root "static_pages#home"
+  get "password_resets/new"
+  get "password_resets/edit"
   get "/signup", to: "users#new"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
